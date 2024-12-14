bae9abec33f47fe005ce83ae82aebe3b6380c752
fransua <fransua.ne@gmail.com>
2024-12-14 21:31:20 +0100

    android 34

--- android/run.sh
+++ android/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 ./answers.sh | bubblewrap init --manifest=https://$1/manifest.json
-bubblewrap build
-mv ./app-release-* /out
+
+export ANDROID_SDK_ROOT=/usr/lib/android
+cat ./app/build.gradle | sed -e s/33/34/g | tee ./app/build.gradle
+./gradlew bundleRelease
+cp ./app/build/outputs/bundle/release/app-release.aab /out/
+jarsigner -keystore ./android.keystore /out/app-release.aab android \
+  -storepass $BUBBLEWRAP_KEYSTORE_PASSWORD \
+  -keypass $BUBBLEWRAP_KEY_PASSWORD
